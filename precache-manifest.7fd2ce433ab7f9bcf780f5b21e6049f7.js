33950f97e6fe97637b91aa63bd07f46236716ed1
pyankovit <pyankov.it@gmail.com>
2019-10-28 22:46:33 +0200

    Updates

---
+++ precache-manifest.7fd2ce433ab7f9bcf780f5b21e6049f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89348852be42d3c316d8578c3812859d",
+    "url": "/crypto-ticker-cards/index.html"
+  },
+  {
+    "revision": "32fd40508d235eb03726",
+    "url": "/crypto-ticker-cards/static/css/main.bc1d4070.chunk.css"
+  },
+  {
+    "revision": "7986dbcdc5cca45e8b35",
+    "url": "/crypto-ticker-cards/static/js/2.110cd2e3.chunk.js"
+  },
+  {
+    "revision": "32fd40508d235eb03726",
+    "url": "/crypto-ticker-cards/static/js/main.6c3ea6d8.chunk.js"
+  },
+  {
+    "revision": "136fa860086ea1b4b4ec",
+    "url": "/crypto-ticker-cards/static/js/runtime-main.95d38aa5.js"
+  }
+]);
